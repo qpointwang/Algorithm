57a1a5344657c434cdc54d47ea63b83fcedbfc5c
wchee3 <qpointwang@163.com>
2020-09-02 20:37:31 +0800

    offer18

---
+++ src/offer/Offer18.java
@@ -0,0 +1,52 @@
+package offer;
+
+
+/**
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+ * <p>
+ * 返回删除后的链表的头节点。
+ * <p>
+ * 注意：此题对比原题有改动
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: head = [4,5,1,9], val = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 示例 2:
+ * <p>
+ * 输入: head = [4,5,1,9], val = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Offer18 {
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode cur = head;
+        if (cur.val == val) {
+            return cur.next;
+        }
+        while (cur.next != null) {
+            System.out.println(cur.val);
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+                break;
+            }
+        }
+        return head;
+    }
+}
